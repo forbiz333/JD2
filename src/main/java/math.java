d81d571955c14f61cd4529f0bb346f29756eefa9
forbiz333 <forbiz333@gmail.com>
2017-03-17 20:37:05 +0300

    multy

--- src/main/java/math.java
+++ src/main/java/math.java
@@ -1,6 +1,14 @@
 /**
  * Created by user on 17.03.2017.
  */
-public class math {
+public class Math {
+
+    double a;
+    double b;
+    double c;
+
+
+    c =a * c;
+    System.out.println(c)
 
 }
